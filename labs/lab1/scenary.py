31b03e4dac72f479258bcb1361ab9582b2603644
lpb247 <lpb3296@rit.edu>
2020-11-25 02:15:18 -0500

    made a pine tree

--- labs/lab1/scenary.py
+++ labs/lab1/scenary.py
@@ -3,6 +3,9 @@
 # Turtle Scenary
 import turtle
 
+"""
+make a maple tree with user inputer size and color
+"""
 def maple_tree(trunk, color):
     top = trunk * .4
 
@@ -17,11 +20,31 @@ def maple_tree(trunk, color):
     turtle.forward(trunk)
     turtle.left(90)
 
+def pine_tree(trunk, color):
+    top = trunk * .6
+
+    turtle.down()
+    turtle.left(90)
+    turtle.color(color)
+    turtle.forward(trunk)
+    turtle.right(90)
+    turtle.forward(top/2)
+    turtle.left(120)
+    turtle.forward(top)
+    turtle.left(120)
+    turtle.forward(top)
+    turtle.left(120)
+    turtle.forward(top/2)
+    turtle.up()
+    turtle.right(90)
+    turtle.forward(trunk)
+    turtle.left(90)
+    turtle.done()
 
 def main():
     trunk = int(input('enter a trunk size: '))
     color = input('please enter a color: ')
-    maple_tree(trunk, color)
+    pine_tree(trunk, color)
 
 
 main()
