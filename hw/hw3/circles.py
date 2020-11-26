28963d1690a4fa09de23fab14301217e14903ecd
lpb247 <lpb3296@rit.edu>
2020-11-25 22:50:02 -0500

    added an option for 2 circles

--- hw/hw3/circles.py
+++ hw/hw3/circles.py
@@ -4,7 +4,7 @@
 
 import turtle
 
-def circle(radius, color):
+def circle1(radius, color):
     turtle.color(color)
     turtle.up()
     turtle.forward(radius)
@@ -15,14 +15,30 @@ def circle(radius, color):
     turtle.up()
     turtle.goto(0,0)
 
+def circle2(radius, color):
+    rad = radius / 2
+    turtle.color(color)
+    turtle.down()
+    turtle.circle(rad)
+    turtle.right(180)
+    turtle.circle(rad)
+    turtle.up()
+    turtle.goto(0,0)
+
 
 def main():
 
     while True:
         try:
+            amt = int(input('how many layers would you like (1 - 3): '))
             radius = int(input('please enter a radius: '))
             color = input('please enter a color: ')
-            circle(radius, color)
+            if amt == 1:
+                circle1(radius, color)
+            if amt == 2:
+                color2 = input('what would you like the second color to be: ')
+                circle1(radius, color)
+                circle2(radius, color2)
             return False
 
         except turtle.TurtleGraphicsError:
