1bba50a2603c7a8b62cdf01142ddab555a25a4af
lpb247 <lpb3296@rit.edu>
2020-11-25 23:27:50 -0500

    finalized the hw

--- hw/hw3/circles.py
+++ hw/hw3/circles.py
@@ -25,6 +25,27 @@ def circle2(radius, color):
     turtle.up()
     turtle.goto(0,0)
 
+def circle3(radius, color):
+    rad = radius / 4
+    turtle.left(90)
+    turtle.forward(radius/2)
+    turtle.color(color)
+    turtle.down()
+
+    turtle.circle(rad)
+    turtle.right(180)
+    turtle.circle(rad)
+    turtle.up()
+    turtle.goto(0,0)
+    turtle.forward(radius/2)
+
+    turtle.down()
+    turtle.circle(rad)
+    turtle.right(180)
+    turtle.circle(rad)
+    turtle.up()
+    turtle.goto(0,0)
+    turtle.done()
 
 def main():
 
@@ -33,12 +54,24 @@ def main():
             amt = int(input('how many layers would you like (1 - 3): '))
             radius = int(input('please enter a radius: '))
             color = input('please enter a color: ')
+
+            if amt == 0:
+                print('no circles for you :(')
+
             if amt == 1:
                 circle1(radius, color)
             if amt == 2:
                 color2 = input('what would you like the second color to be: ')
+
                 circle1(radius, color)
                 circle2(radius, color2)
+            if amt == 3:
+                color2 = input('what would you like the second color to be: ')
+                color3 = input('what would you like the third color to be:  ')
+
+                circle1(radius, color)
+                circle2(radius, color2)
+                circle3(radius, color3)
             return False
 
         except turtle.TurtleGraphicsError:
