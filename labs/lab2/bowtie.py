f6ba7183b2038873d1b641622841b7a0c9bac440
lpb247 <lpb3296@rit.edu>
2020-11-27 18:21:12 -0500

    progress

--- labs/lab2/bowtie.py
+++ labs/lab2/bowtie.py
@@ -27,94 +27,99 @@ def bowtie(len):
     turtle.begin_fill()
     turtle.circle(len/4)
     turtle.end_fill()
+    turtle.up()
 
     turtle.right(90)
     turtle.back(len/4)
-    turtle.up()
 
-def rec_bowtie1(len):
-    bowtie(len)
+def rec_bowtie1(len, depth):
+    if depth > 0:
+        turtle.speed(0)
+        print('depth = ' + str(depth))
+        bowtie(len)
 
-    turtle.left(30)
-    turtle.forward(len*2)
-    turtle.right(30)
-    turtle.down()
+        turtle.up()
+        turtle.left(30)
+        turtle.forward(len*2.5)
+        turtle.down()
 
-    bowtie(len/2)
+        rec_bowtie1(len/2, depth-1)
 
-    turtle.left(30)
-    turtle.back(len*2)
-    turtle.right(60)
-    turtle.forward(len*2)
-    turtle.left(30)
-    turtle.down()
+        turtle.up()
+        turtle.back(len*2.5)
+        turtle.right(60)
+        turtle.forward(len*2.5)
+        turtle.down()
 
-    bowtie(len/2)
+        rec_bowtie1(len/2, depth-1)
 
-    turtle.right(30)
-    turtle.back(len*2)
-    turtle.left(180)
-    turtle.forward(len*2)
-    turtle.right(150)
-    turtle.down()
+        turtle.up()
+        turtle.back(len*2.5)
+        turtle.left(180)
+        turtle.forward(len*2.5)
+        turtle.down()
 
-    bowtie(len/2)
+        rec_bowtie1(len/2, depth-1)
 
-    turtle.right(30)
-    turtle.forward(len*2)
-    turtle.right(120)
-    turtle.forward(len*2)
-    turtle.left(150)
-    turtle.down()
+        turtle.up()
+        turtle.forward(len*2.5)
+        turtle.right(120)
+        turtle.forward(len*2.5)
+        turtle.down()
 
-    bowtie(len/2)
+        rec_bowtie1(len/2, depth-1)
 
-    turtle.left(30)
-    turtle.forward(len*2)
-    turtle.right(30)
+        turtle.up()
+        turtle.left(30)
+        turtle.forward(len*2.5)
+        turtle.right(30)
+        turtle.down()
+        turtle.down()
 
-def rec_bowtie2(len):
-    turtle.speed(0)
-    bowtie(len)
 
-    turtle.left(30)
-    turtle.forward(len * 2.5)
-    turtle.right(30)
-    turtle.down()
 
-    rec_bowtie1(len/2)
-
-    turtle.left(30)
-    turtle.back(len * 2.5)
-    turtle.right(60)
-    turtle.forward(len * 2.5)
-    turtle.left(30)
-    turtle.down()
-
-    rec_bowtie1(len/2)
-
-    turtle.right(30)
-    turtle.back(len * 2.5)
-    turtle.left(180)
-    turtle.forward(len * 2.5)
-    turtle.right(150)
-    turtle.down()
-
-    rec_bowtie1(len/2)
-
-    turtle.right(30)
-    turtle.forward(len*2.5)
-    turtle.right(120)
-    turtle.forward(len*2.5)
-    turtle.left(150)
-    turtle.down()
-
-    rec_bowtie1(len/2)
-    turtle.done()
+# def rec_bowtie2(len):
+#     turtle.speed(0)
+#     bowtie(len)
+#
+#     turtle.left(30)
+#     turtle.forward(len * 2.5)
+#     turtle.right(30)
+#     turtle.down()
+#
+#     rec_bowtie1(len/2)
+#
+#     turtle.left(30)
+#     turtle.back(len * 2.5)
+#     turtle.right(60)
+#     turtle.forward(len * 2.5)
+#     turtle.left(30)
+#     turtle.down()
+#
+#     rec_bowtie1(len/2)
+#
+#     turtle.right(30)
+#     turtle.back(len * 2.5)
+#     turtle.left(180)
+#     turtle.forward(len * 2.5)
+#     turtle.right(150)
+#     turtle.down()
+#
+#     rec_bowtie1(len/2)
+#
+#     turtle.right(30)
+#     turtle.forward(len*2.5)
+#     turtle.right(120)
+#     turtle.forward(len*2.5)
+#     turtle.left(150)
+#     turtle.down()
+#
+#     rec_bowtie1(len/2)
+#     turtle.done()
 
 
 def main():
     len = int(input('please enter a length: '))
-    rec_bowtie2(len)
+    rec_bowtie1(len, 2)
 
 main()
